ea71723436b3ed93f61ae03586e5905dfac8e1a9
Pavan Itagi <pavan.itagi@ymedialabs.com>
2019-01-30 14:28:54 +0530

    Setting up profile Viewset

--- src/shopping_project/shopping_api/views.py
+++ src/shopping_project/shopping_api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
+
+from . import serializers
+from . import models
+from . import permissions
+
 # Create your views here.
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handles creating, reading and updating profiles"""
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email')
